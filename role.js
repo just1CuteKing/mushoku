35b3b6e96e7abe784e93fe24e160bc245ba5e10d
nono <136939713+nourelaino@users.noreply.github.com>
2024-02-14 01:14:35 +0100

    gg

--- role.js
+++ role.js
@@ -1,7 +1,7 @@
 function randomWifu() {
     const probabilities = {
-      roxy: 0.50, // 50% chance
-      selfy: 0.50 // 50% chance
+      roxy: 0.5, // 50% chance
+      selfy: 0.5 // 50% chance
     };
   
     const randomNumber = Math.random();
@@ -23,16 +23,37 @@ function randomWifu() {
     videoPlayer.style.display = 'block';
     videoPlayer.play();
   });
+
   
-  const div = document.querySelector('.F-div');
-  const div2 = document.querySelector('.S-div');
-  const div3 = document.querySelector('.T-div');
-  const div4 =  document.querySelector('.five-div');
-  const name = document.createElement('h4');
-  const image = document.createElement('img');
-  const image2 = document.createElement('img');
-  const gifImage = document.createElement('img');
-  const tryAgainButton = document.createElement('button');
+const div = document.querySelector('.F-div');
+const div2 = document.querySelector('.S-div');
+const div3 = document.querySelector('.T-div');
+const div4 =  document.querySelector('.five-div');
+const div6 =  document.querySelector('.sex-div');
+const name = document.createElement('h4');
+const image = document.createElement('img');
+const image2 = document.createElement('img');
+const image3 = document.createElement('img');
+const gifImage = document.createElement('img');
+const tryAgainButton = document.createElement('button');
+  
+
+
+function resetGame() {
+    div3.removeChild(image);
+    div.removeChild(gifImage);
+    document.body.removeChild(tryAgainButton);
+    div2.removeChild(name);
+    div4.removeChild(image2);
+    div6.removeChild(image3);
+
+    playButton.style.display = 'block';
+    document.body.style.backgroundImage = 'url("696c1346c27df162ecd95129ff4ea552.jpg")';
+
+    videoPlayer.addEventListener('ended', handleVideoEnd);
+  };
+  
+
   
 function handleVideoEnd(){
     videoPlayer.style.display = 'none';
@@ -48,14 +69,15 @@ function handleVideoEnd(){
   
     image.src = `../your wifu is runnig from u/imag/${gg}/${gg}.png`;
     image2.src = `../your wifu is runnig from u/imag/${gg}/${gg}2.png`;
+    image3.src = `../your wifu is runnig from u/imag/${gg}/${gg}3.png`;
     image.classList.add('animate__animated', 'animate__fadeInBottomRight');
     image.classList.add('image');
+    image2.classList.add('animate__animated', 'animate__fadeInBottomRight' , 'image2');
+    image3.classList.add('animate__animated', 'animate__fadeInBottomRight', 'image3');
 
-    if (gg !== 'roxy') {
-        image2.classList.add('animate__animated', 'animate__fadeInBottomRight');
-        image2.classList.add('image');
-    } else if (gg === 'roxy') {
+   if (gg === 'roxy') {
         image2.src = '';
+        image3.src = '';
     }
     gifImage.src = `../your wifu is runnig from u/gif/${gg}.gif`;
     gifImage.style.width = '300px';
@@ -67,25 +89,18 @@ function handleVideoEnd(){
     div2.appendChild(name);
     div3.appendChild(image);
     div4.appendChild(image2);
-
+    div6.appendChild(image3);
 
 
     tryAgainButton.textContent = 'Try Again';
     tryAgainButton.classList.add('try-again-button');
-  
-    tryAgainButton.addEventListener('click', function () {
-      div3.removeChild(image);
-      div.removeChild(gifImage);
-      document.body.removeChild(tryAgainButton);
-      div2.removeChild(name);
-      div4.removeChild(image2)
-  
-      playButton.style.display = 'block';
-      document.body.style.backgroundImage = 'url("696c1346c27df162ecd95129ff4ea552.jpg")';
-  
-      videoPlayer.addEventListener('ended', handleVideoEnd);
-    });
-  
+
+    tryAgainButton.addEventListener('click',resetGame);
+
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {resetGame()}});
+
     document.body.appendChild(tryAgainButton);
 }
   
