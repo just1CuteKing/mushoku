6304397197f5c636b9e090273e88eab2a82018d3
nono <136939713+nourelaino@users.noreply.github.com>
2024-02-15 14:15:03 +0100

    fr

--- role.js
+++ role.js
@@ -1,35 +1,42 @@
 function randomWifu() {
   const probabilities = {
-   zenith: 0.05,//done
-    aisha: 0.05,//done 
-    ariel: 0.05,//done
-    elinalise: 0.05,//done
-    eris: 0.05,//done
-    ghislaine: 0.05,//done
-    hilda: 0.05,//done
-    juliette: 0.05,//done
-    kishirika: 0.05,//done
-    linia: 0.05,//done
-    nanahoshi: 0.05,//done
-    norn: 1,// done
-    pursena: 0.05, //done 
-    rokari: 0.05,//done
-    roxy: 0.05,//done
-    suzanne: 0.05,//done
-    sylphiette: 0.05,//done
-   /* lilia: 0.05,// not done yet 
-    nina: 0.05,// not dont yet
-    sara: 0.05,*/
+      zenith: 0.05,
+      aisha: 0.05,
+      ariel: 0.05,
+      elinalise: 0.05,
+      eris: 0.05,
+      ghislaine: 0.05,
+      hilda: 0.05,
+      juliette: 0.05,
+      kishirika: 0.05,
+      linia: 0.05,
+      nanahoshi: 0.05,
+      norn: 0.05,
+      pursena: 0.05,
+      rokari: 0.05,
+      roxy: 0.05,
+      suzanne:0.05,
+      sylphiette: 0.05,
+      // lilia: 0.05, // not done yet
+      // nina: 0.05, // not done yet
+      // sara: 0.05, // not done yet
   };
 
-    const randomNumber = Math.random();
-    let cumulativeProbability = 0;
-    for (const [randomWifu, probability] of Object.entries(probabilities)) {
+
+  const randomNumber = Math.random();
+  console.log('R:'+randomNumber)
+  let cumulativeProbability = 0;
+
+  for (const [randomWifu, probability] of Object.entries(probabilities)) {
       cumulativeProbability += probability;
+      console.log('p:'+probability)
+      console.log('c:'+cumulativeProbability)
       if (randomNumber < cumulativeProbability) {
-        return randomWifu;
+          return randomWifu;
       }
-    }
+  }
+  // If no wifu is selected, return a default or handle it as needed
+  return "pursena";
 }
 
 const playButton = document.getElementById('playButton');
@@ -40,6 +47,8 @@ playButton.addEventListener('click', function () {
     playButton.style.display = 'none';
     videoPlayer.style.display = 'block';
     videoPlayer.play();
+    
+console.log(randomWifu())
   });
 
 const div = document.querySelector('.F-div');
@@ -101,56 +110,53 @@ function handleVideoEnd(){
     gifImage.style.backgroundRepeat = 'no-repeat';
     gifImage.classList.add('animate__animated', 'animate__fadeInBottomRight');
 
-    if (gg === 'roxy' || gg == 'zenith') {
+  if (gg === 'roxy' || gg == 'zenith') {
       image2.src = '';
       image3.src = '';
-    }else if (gg === 'eris'|| gg === 'elinalise' || gg ==='juliette') {
-    image.src = '';
-    image3.src = '';
-    gifImage.src ='';
-    }
-   if(gg ==='juliette') {
-    image.src ='';
-    image3.src = '';
-    gifImage.src ='';
-    }
-     if (gg === 'ariel') {
-    gifImage.src = ``;
-    document.body.style.backgroundImage = `url("../your wifu is runnig from u/back-image/back-${gg}.png")`;
-    }
-     if(gg === 'aisha'){
-      image.src = '';
-      image2.src = '';
-      image3.src = '';
-      gifImage.src = ``;
-      document.body.style.backgroundImage = ``;
-      videoPlayer02.style.display = 'block';
-      videoPlayer02.style.visibility = 'visible'
-      videoPlayer02.play();
-    }
-    if(gg === 'ghislaine' || gg === 'kishirika'){
-      image.src ='';
-      image2.src ='';
-      image3.src ='';
-    }
-    if(gg === 'hilda' || gg === 'pursena'){
-      image.src ='';
-      image3.src ='';
-      gifImage.src ='';
-    document.body.style.backgroundImage = `url("../your wifu is runnig from u/back-image/back-${gg}.png")`;
-    }
-    if(gg === 'norn'){
-      gifImage.src = '';
-      image3.src=''
+    } else if (gg === 'eris'|| gg === 'elinalise' || gg ==='juliette') {
+          image.src = '';
+          image3.src = '';
+          gifImage.src ='';
+        }else if(gg ==='juliette') {
+            image.src ='';
+            image3.src = '';
+            gifImage.src ='';
+          }else if (gg === 'ariel' || gg === 'suzanne') {
+              gifImage.src = `../your wifu is runnig from u/gif/${gg}.png`;
+              document.body.style.backgroundImage = `url("../your wifu is runnig from u/back-image/back-${gg}.png")`;
+            }else if (gg === 'ariel') {
+                gifImage.src = `../your wifu is runnig from u/gif/${gg}.png`;
+                document.body.style.backgroundImage = `url("../your wifu is runnig from u/back-image/back-${gg}.png")`;
+              }else if(gg === 'aisha'){
+                  image.src = '';
+                  image2.src = '';
+                  image3.src = '';
+                  gifImage.src = ``;
+                  document.body.style.backgroundImage = ``;
+                  videoPlayer02.style.display = 'block';
+                  videoPlayer02.style.visibility = 'visible'
+                  videoPlayer02.play();
+                }else if(gg === 'ghislaine' || gg === 'kishirika'){
+                    image.src ='';
+                    image2.src ='';
+                    image3.src ='';
+                  }else if(gg === 'hilda' || gg === 'pursena'){
+                      image.src ='';
+                      image3.src ='';
+                      gifImage.src ='';
+                      document.body.style.backgroundImage = `url("../your wifu is runnig from u/back-image/back-${gg}.png")`;
+                    }else if(gg === 'norn'){
+                        gifImage.src = '';
+                        image3.src=''
 
-      }
-    if( gg === 'rokari'){
-      gifImage.src = '';
-      image.src ='';
-      }
-      if( gg === 'nanahoshi'){
-        image.src ='';
-        }
+                      }else if( gg === 'rokari'){
+                          gifImage.src = '';
+                          image.src ='';
+                       }else if( gg === 'nanahoshi'){
+                            image.src ='';
+                         }else if(gg == 'linia'){
+                              image.src ='';
+                           }
 
     div.appendChild(gifImage);
     div2.appendChild(name);
